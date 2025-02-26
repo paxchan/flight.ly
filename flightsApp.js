5cfa87cd70c035b2a37137bb9d920a7444a4e562
Channing Paxman <channing.paxman@gmail.com>
2025-02-26 00:37:55 -0700

    accept traffic

--- flightsApp.js
+++ flightsApp.js
@@ -180,6 +180,6 @@ app.get('/', async (req, res) => {
 
 // Start server
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
